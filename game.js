769590b04ad951db138e5f98211878b48edf25a1
Julia <yulyakonoval1998@gmail.com>
2020-06-23 20:25:43 +0300

    new image was styled

--- game.js
+++ game.js
@@ -1,34 +1,41 @@
-var things = ["rock", "paper", "scissors"];
+var options = ["rock", "paper", "scissors"];
 var results = 0;
-let playerSelection = prompt("Rock, paper, scissors?");
-let computerSelection = function computerPlay() {
-  return things[Math.floor(Math.random() * things.length)];
-};
-console.log(computerSelection);
 
-function playRound(playerSelection, computerSelection) {
+function playRound(computerSelection) {
+  playerSelection = prompt("Rock, paper, scissors?");
   playerSelection = playerSelection.toLowerCase();
-  if (playerSelection == "rock" && computerSelection == "paper") {
+  if (playerSelection === "rock" && computerSelection === "paper") {
     alert("You Lose! Paper beats Rock");
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
     alert("You win! Paper beats Rock");
-  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    results++;
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
     alert("You Lose! Paper beats Rock");
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
     alert("You win! Scissors beat paper");
-  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    results++;
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
     alert("You Lose! Rock beats Scissors");
-  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
     alert("You win! Scissors  beat rock");
+    results++;
+  } else {
+    alert("Its a draw!");
   }
 }
-for (var i = 0; i < 6; i++) {
-  function game(playRound) {
-    if ((alert = "You win!")) {
-      return results + 1;
-    } else {
-      return results;
-    }
+
+function displayScore() {
+  document.querySelector("#player-score").textContent = results;
+}
+
+function game() {
+  for (var i = 0; i < 6; i++) {
+    let computerSelection = options[Math.floor(Math.random() * options.length)];
+    console.log(computerSelection);
+    displayScore();
+    playRound(computerSelection);
   }
 }
+
+game();
 alert(results);
